525e76d765e2249a628f117dc6285af3f1840a84
Syed Fawzul Azim <adnansyed27@gmail.com>
2022-03-15 22:55:40 +0100

    about structure done

--- frontend_react/src/container/About/About.tsx
+++ frontend_react/src/container/About/About.tsx
@@ -1,8 +1,41 @@
 import React from "react";
+import { motion } from "framer-motion";
+
 import "./About.scss";
+import { abouts } from "../../constants";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <>
+      <h2 className="head-text">
+        I know that
+        <span>Good Design</span>
+        <br />
+        means
+        <span>Good Business</span>
+      </h2>
+
+      <div className="app__profiles">
+        {abouts.map((about, index) => (
+          <motion.div
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: "tween" }}
+            className="app__profile-item"
+            key={about.title + index}
+          >
+            <img src={about.imageURL} alt={about.title} />
+            <h2 className="bold-text" style={{ marginTop: 20 }}>
+              {about.title}
+            </h2>
+            <h2 className="bold-text" style={{ marginTop: 10 }}>
+              {about.description}
+            </h2>
+          </motion.div>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default About;
