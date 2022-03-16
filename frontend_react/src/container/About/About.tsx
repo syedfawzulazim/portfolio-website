9651703dc9bd75c37c5159fb725ece609674f61b
Syed Fawzul Azim <adnansyed27@gmail.com>
2022-03-16 10:27:03 +0100

    about section done

--- frontend_react/src/container/About/About.tsx
+++ frontend_react/src/container/About/About.tsx
@@ -1,36 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 
 import "./About.scss";
-import { abouts } from "../../constants";
+import { urlFor, client } from "../../client";
+
+interface dataObj {
+  title: string;
+  description: string;
+  imgUrl: string;
+}
 
 const About = () => {
+  const [abouts, setAbouts] = useState<dataObj[]>([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query).then((data: dataObj[]) => {
+      setAbouts(data);
+    });
+  }, []);
+
+  console.log(abouts);
   return (
     <>
       <h2 className="head-text">
-        I know that
-        <span>Good Design</span>
-        <br />
-        means
+        I know that <span>Good Applications</span> <br /> means{" "}
         <span>Good Business</span>
       </h2>
 
       <div className="app__profiles">
         {abouts.map((about, index) => (
           <motion.div
-            whileInView={{ opacity: 1 }}
+            whileInView={{ opacity: [0, 1], x: [-100, 0] }}
             whileHover={{ scale: 1.1 }}
+            // animate={{ x: [-100, 0] }}
             transition={{ duration: 0.5, type: "tween" }}
             className="app__profile-item"
             key={about.title + index}
           >
-            <img src={about.imageURL} alt={about.title} />
+            <img src={urlFor(about.imgUrl).toString()} alt={about.title} />
             <h2 className="bold-text" style={{ marginTop: 20 }}>
               {about.title}
             </h2>
-            <h2 className="bold-text" style={{ marginTop: 10 }}>
-              {about.description}
-            </h2>
+            <p style={{ marginTop: 10 }}>{about.description}</p>
           </motion.div>
         ))}
       </div>
