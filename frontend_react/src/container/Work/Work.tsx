ded18d2ff1f38f3159a800c0afe53b78d3c4dd5f
Syed Fawzul Azim <adnansyed27@gmail.com>
2022-03-20 08:35:34 +0100

    work section added

--- frontend_react/src/container/Work/Work.tsx
+++ frontend_react/src/container/Work/Work.tsx
@@ -1,8 +1,77 @@
-import React from "react";
-import "./Work.scss";
+import React, { useState, useEffect } from "react";
+import { AiFillEye, AiFillGithub } from "react-icons/ai";
+import { motion } from "framer-motion";
+import { AppWrap } from "../../wrapper";
 
+import "./Work.scss";
+import { urlFor, client } from "../../client";
+
+interface dataObjType {
+  title: string;
+  description: string;
+  imgUrl: string;
+  tags: [];
+}
 const Work = () => {
-  return <div>Work</div>;
+  const [activeFilter, setActiveFilter] = useState("All");
+  const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
+  const [works, setWorks] = useState([]);
+  const [filterWork, setFilterWork] = useState<dataObjType[]>([]);
+
+  useEffect(() => {
+    const query = '*[_type == "works"]';
+
+    client.fetch(query).then((data) => {
+      setWorks(data);
+      setFilterWork(data);
+    });
+  }, []);
+
+  console.log(filterWork);
+  const handleWorkFilter = (item: string) => {};
+
+  return (
+    <>
+      <h2 className="head-text">
+        My creative <span>Portfolio</span> Section
+      </h2>
+      <div className="app__work-filter">
+        {["UI/UX", "Web App", "React JS", "All"].map((item, index) => (
+          <div
+            key={index}
+            onClick={() => handleWorkFilter(item)}
+            className={`app__work-filter-item app__flex p-text ${
+              activeFilter === item ? "item-active" : ""
+            }`}
+          >
+            {item}
+          </div>
+        ))}
+      </div>
+      <motion.div
+        animate={animateCard}
+        transition={{ duration: 0.5, delayChildren: 0.5 }}
+        className="app__work-portfolio"
+      >
+        {filterWork.map((work, index) => (
+          <div className="app__work-item app__flex" key={index}>
+            <div className="app__work-img app__flex">
+              <img src={urlFor(work.imgUrl).toString()} alt={work.title} />
+              <motion.div
+                whileHover={{ opacity: [0, 1] }}
+                transition={{
+                  duration: 0.25,
+                  ease: "easeInOut",
+                  staggerChildren: 0.5,
+                }}
+                className="app__work-hover app__flex"
+              ></motion.div>
+            </div>
+          </div>
+        ))}
+      </motion.div>
+    </>
+  );
 };
 
 export default Work;
