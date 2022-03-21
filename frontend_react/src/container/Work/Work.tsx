9db00e60ee111f5023540cb9570ad4fcea37e1ac
Syed Fawzul Azim <adnansyed27@gmail.com>
2022-03-21 21:32:56 +0100

    work section added

--- frontend_react/src/container/Work/Work.tsx
+++ frontend_react/src/container/Work/Work.tsx
@@ -10,7 +10,9 @@ interface dataObjType {
   title: string;
   description: string;
   imgUrl: string;
-  tags: [];
+  tags?: string[];
+  projectLink: string;
+  codeLink: string;
 }
 const Work = () => {
   const [activeFilter, setActiveFilter] = useState("All");
@@ -25,9 +27,9 @@ const Work = () => {
       setWorks(data);
       setFilterWork(data);
     });
+    console.log(filterWork);
   }, []);
 
-  console.log(filterWork);
   const handleWorkFilter = (item: string) => {};
 
   return (
@@ -65,7 +67,45 @@ const Work = () => {
                   staggerChildren: 0.5,
                 }}
                 className="app__work-hover app__flex"
-              ></motion.div>
+              >
+                <a href={work.projectLink} target="_blank" rel="noreferrer">
+                  <motion.div
+                    whileInView={{ scale: [0, 1] }}
+                    whileHover={{ scale: [1, 0.9] }}
+                    transition={{
+                      duration: 0.25,
+                      ease: "easeInOut",
+                      staggerChildren: 0.5,
+                    }}
+                    className="app__flex"
+                  >
+                    <AiFillEye />
+                  </motion.div>
+                </a>
+                <a href={work.projectLink} target="_blank" rel="noreferrer">
+                  <motion.div
+                    whileInView={{ scale: [0, 1] }}
+                    whileHover={{ scale: [1, 0.9] }}
+                    transition={{
+                      duration: 0.25,
+                      ease: "easeInOut",
+                      staggerChildren: 0.5,
+                    }}
+                    className="app__flex"
+                  >
+                    <AiFillGithub />
+                  </motion.div>
+                </a>
+              </motion.div>
+            </div>
+            <div className="app__work-content app__flex">
+              <h4 className="bold-text">{work.title}</h4>
+              <p className="p-text" style={{ marginTop: 10 }}>
+                {work.description}
+              </p>
+              <div className="app__work-tag app__flex">
+                <p className="p-text">{works.tags[0]}</p>
+              </div>
             </div>
           </div>
         ))}
