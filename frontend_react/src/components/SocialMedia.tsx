273d15771625cbebd5227cfd4212cb95517e9419
Syed Fawzul Azim <adnansyed27@gmail.com>
2022-03-27 13:55:05 +0200

    links added for social media

--- frontend_react/src/components/SocialMedia.tsx
+++ frontend_react/src/components/SocialMedia.tsx
@@ -1,18 +1,26 @@
 import React from "react";
-import { BsTwitter, BsInstagram } from "react-icons/bs";
-import { FaFacebookF } from "react-icons/fa";
+import { BsGithub, BsLinkedin } from "react-icons/bs";
 
 const SocialMedia = () => {
   return (
     <div className="app__social">
       <div>
-        <BsTwitter />
+        <a
+          href="https://www.linkedin.com/in/syed-fawzul-azim-332a92a4/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <BsLinkedin />
+        </a>
       </div>
       <div>
-        <FaFacebookF />
-      </div>
-      <div>
-        <BsInstagram />
+        <a
+          href="https://github.com/syedfawzulazim"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <BsGithub />
+        </a>
       </div>
     </div>
   );
